4d8591d15f1d96d08f517839946a097ce027c743
rdhwan <rdhwan@users.noreply.github.com>
2023-03-25 04:52:12 +0000

    deploy: 591b6cb910ce0cf68dfd23c176412f764a711d74

---
+++ _next/static/08vFGYAQBedgOMwroOATn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/725-c5937870fdb51dc9.js","static/chunks/pages/index-efa9d6811b4cec34.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
